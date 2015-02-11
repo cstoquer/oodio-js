c9e93cf9e18a8f577b29647e41ba3b3ea9b2fea3
Cedric Stoquer <cstoquer@wizcorp.jp>
2015-02-11 21:56:26 +0900

    volume is now a public attribute

--- src/Mix1_1A.js
+++ src/Mix1_1A.js
@@ -11,9 +11,9 @@ function Mix1_1A(params) {
 	this.input1 = [0.0];
 	this.input2 = [0.0];
 	this.out    = [0.0];
-	this._vol   = params.volume !== undefined ? params.volume : 0.0;
+	this.volume = params.volume !== undefined ? params.volume : 0.0;
 }
 
 Mix1_1A.prototype.tic = function () {
-	this.out[0] = this.input1[0] + this.input2[0] * this._vol;
+	this.out[0] = this.input1[0] + this.input2[0] * this.volume;
 };
