974b81b6bf9040333de5388fca4b8bee607fe51d
Cedric Stoquer <cstoquer@wizcorp.jp>
2015-07-05 21:26:34 +0900

    todo

--- src/core/EventInConnector.js
+++ src/core/EventInConnector.js
@@ -10,5 +10,7 @@ EventInConnector.prototype.connectorClassName = 'eventIn';
 EventInConnector.prototype.color = ROOT.COLOR.EVENT;
 
 EventInConnector.prototype.connect = function (connector) {
+	// TODO: check connector type.
+	// there is a "Maximum call stack size exceeded" bug if we try to connect 2 EventIn Connectors
 	connector.connect(this);
 };
